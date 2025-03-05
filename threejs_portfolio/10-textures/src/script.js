a0a6c003b21407064f2bcaec516db7d46c2d2de9
Américo <84201638+AmericoRoca@users.noreply.github.com>
2025-03-05 17:26:01 +0100

    textures

--- threejs_portfolio/10-textures/src/script.js
+++ threejs_portfolio/10-textures/src/script.js
@@ -5,6 +5,46 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
  * Base
  */
 // Canvas
+const loadingManager = new THREE.LoadingManager()
+
+loadingManager.onStart = () =>{
+    console.log("start");
+    
+}
+
+loadingManager.onLoad = () =>{
+    console.log("load");
+}
+
+loadingManager.onProgress = () =>{
+    console.log("progress");
+}
+
+loadingManager.onError = () =>{
+    console.log("error");
+}
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+const colorTexture = textureLoader.load("/textures/door/color.jpg",);
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg",);
+const heightTexture = textureLoader.load("/textures/door/height.jpg",);
+const normalTexture = textureLoader.load("/textures/door/normal.jpg",);
+const ambientOclussionTexture = textureLoader.load("/textures/door/ambientOcclusion.jpg",);
+const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg",);
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg",);
+
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 2
+// colorTexture.wrapS = THREE.RepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping
+
+
+// colorTexture.offset.x = 0.5
+
+// colorTexture.rotation = 1 
+
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
@@ -14,7 +54,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
