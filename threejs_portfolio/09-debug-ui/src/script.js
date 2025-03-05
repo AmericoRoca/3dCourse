47b0ee44033d85c223857da4f9f281ea080a3cd4
Américo <84201638+AmericoRoca@users.noreply.github.com>
2025-03-05 12:47:18 +0100

    dubgui

--- threejs_portfolio/09-debug-ui/src/script.js
+++ threejs_portfolio/09-debug-ui/src/script.js
@@ -1,7 +1,29 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import GUI from "lil-gui"
 
+const gui = new GUI(
+    {
+        width: 340,
+        title: "GUI test",
+        closeFolders: true
+    }
+)
+
+gui.hide()
+
+
+window.addEventListener("keydown", () =>{
+
+    if(event.key == "h"){
+        gui.show(gui._hidden)
+    }
+})
+
+const debugObject = {
+
+}
 /**
  * Base
  */
@@ -14,10 +36,64 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+debugObject.color = "#3e6ea6"
+
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+const cubeTweaks = gui.addFolder("Awesome cube")
+cubeTweaks.close() //para que se muestre cerrado por defecto
+
+gui
+.add(mesh.position, "y")
+.min(-3)
+.max(3)
+.step(0.01)
+.name("elevation")
+
+
+gui
+.add(mesh, "visible")
+
+
+gui
+.add(material, "wireframe")
+
+gui
+.addColor(debugObject, "color")
+.onChange(() =>{
+
+    material.color.set(debugObject.color)
+    
+})
+
+debugObject.spin = () =>{
+    gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2})
+}
+
+
+//Creamos un folder
+cubeTweaks
+.add(debugObject, "spin")
+
+
+debugObject.subdivision = 2
+
+gui
+.add(debugObject, "subdivision")
+.min(1)
+.max(20)
+.step(1)
+.onFinishChange(()=>{
+
+    mesh.geometry.dispose()
+    mesh.geometry = new THREE.BoxGeometry(
+        1, 1, 1,
+        debugObject.subdivision, debugObject.subdivision, debugObject.subdivision
+    )
+})
 
 /**
  * Sizes
