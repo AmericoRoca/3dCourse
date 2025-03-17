8303ec1a847ef275078c87b25be6e3c83803770d
Américo <americochiclana@gmail.com>
2025-03-17 13:43:21 +0100

    middle of the lesson

--- threejs_portfolio/15-shadows/src/script.js
+++ threejs_portfolio/15-shadows/src/script.js
@@ -18,18 +18,50 @@ const scene = new THREE.Scene()
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
 gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
 directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
+
+directionalLight.castShadow = true
+
+directionalLight.shadow.mapSize.width = 1024 
+directionalLight.shadow.mapSize.height = 1024
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.left = -2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 6
+
+directionalLight.shadow.radius = 10
+
+const camerahelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+camerahelper.visible = false
+scene.add(camerahelper)
+
+//sPOTLIGHT
+const spotLight = new THREE.SpotLight(0xffffff, 3.6, 10, Math.PI * 0.3)
+
+spotLight.castShadow = true
+spotLight.shadow.mapSize.width = 1024
+spotLight.shadow.mapSize.height = 1024
+
+spotLight.castShadow = true
+spotLight.position.set(0, 2,2)
+scene.add(spotLight)
+scene.add(spotLight.target)
+
+const camerasportehelper =  new THREE.CameraHelper(spotLight.shadow.camera)
+scene.add(camerasportehelper)
 
 /**
  * Materials
@@ -47,12 +79,16 @@ const sphere = new THREE.Mesh(
     material
 )
 
+sphere.castShadow = true
+
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
+
+plane.receiveShadow = true
 
 scene.add(sphere, plane)
 
@@ -101,6 +137,9 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.shadowMap.enabled = true
+//renderer.shadowMap.type = THREE.PCFShadowMap
 
 /**
  * Animate
